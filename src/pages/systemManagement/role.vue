080fde6730f6232a6f71f97e37295465acf59848
zhangxiaoxi <brookvip@foxmail.com>
2022-04-06 11:49:13 +0800

    4.6

--- src/pages/systemManagement/role.vue
+++ src/pages/systemManagement/role.vue
@@ -1,13 +1,195 @@
 <template>
+    <div class='role'>
+        <div class='role-top-header flex items-center'>
+            <div class='flex items-center ml-15'>
+                <img style='width: 22px;height: 21px' src='../../assets/img/mark.png'>
+            </div>
+            <h3 class='ml-5'>权限管理</h3>
+        </div>
+        <div class='flex search'>
+            <el-input style='width: 200px' class='ml-20 mr-20' placeholder='请输入账户名'></el-input>
+            <el-button @click='addRole' type='primary' icon='el-icon-plus'>新增角色</el-button>
+        </div>
+        <div class='table-box box-sizing mt-20'>
+            <el-table :data='tableData' border>
+                <el-table-column min-width='96px' prop='roleName' align='center' label='角色名称'></el-table-column>
+                <el-table-column min-width='115px' prop='roleName' align='center' sortable label='状态'>
+                    <template slot-scope='{row}'>
+                        <div class='flex row-center'>
+                            <div v-if='row.open === true' class='switch flex items-center'>
+                                <div class='open'></div>
+                                <div>已开启</div>
 
+                            </div>
+                            <div v-else class='switch flex items-center'>
+                                <div class='close'></div>
+                                <div>已关闭</div>
+                            </div>
+                        </div>
+                    </template>
+                </el-table-column>
+                <el-table-column min-width='185px' prop='roleName' align='center' sortable label='创建时间'></el-table-column>
+                <el-table-column min-width='127px' prop='roleName' align='center' label='创建人'></el-table-column>
+                <el-table-column min-width='429px' prop='roleName' label='说明'></el-table-column>
+                <el-table-column prop='roleName' label='操作'>
+                    <template slot-scope='{row}'>
+                        <div class='flex space-between'>
+                            <el-switch @change='switchChange($event,row)' active-color='#2A79EE'
+                                       inactive-color='#C0C4CC' v-model='row.open'></el-switch>
+                            <span class='pointer but' @click='handleUpdate(row)'>修改</span>
+                            <span class='pointer but' @click='del(row.id)'>删除</span>
+                        </div>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
+        <div class='pagination flex row-center items-center'>
+            <el-pagination
+                @size-change='getData'
+                :page-size='query.size'
+                :current-page='query.page'
+                layout='total,prev, pager, next, jumper'
+                :total='1'>
+            </el-pagination>
+        </div>
+        <el-dialog width='700px' title='角色编辑' :visible.sync='dialogFormVisible'>
+            <el-form :rules="rules" label-position='right' label-width='70px' :model='form'>
+                <div class='flex space-between'>
+                    <el-form-item label='角色名' prop='name'>
+                        <el-input style='width: 176px' placeholder='请输入角色名'></el-input>
+                    </el-form-item>
+                    <el-form-item label='说明'>
+                        <el-input style='width: 187px' placeholder='请输入说明'></el-input>
+                    </el-form-item>
+                </div>
+
+                <el-form-item label='权限'>
+                    <el-select style='width: 100%' v-model="form.select" placeholder="请选择">
+                        <el-option v-for='item in roleList' :label='item.label' :value='item.value' :key='item.value'></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label='人员管理'>
+                    <el-transfer v-model="form.value" :data="data"></el-transfer>
+                </el-form-item>
+            </el-form>
+            <span slot='footer' class='dialog-footer'>
+                <el-button class='cancel' @click='dialogFormVisible = false'>取 消</el-button>
+                <el-button type='primary' @click='isUpdate ? updata() :save()'>确 定</el-button>
+              </span>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'role'
+    name: 'role',
+    data() {
+        const generateData = _ => {
+            const data = [];
+            for (let i = 1; i <= 15; i++) {
+                data.push({
+                    key: i,
+                    label: `备选项 ${ i }`
+                });
+            }
+            return data;
+        };
+        return {
+            tableData: [],
+            query: {
+                size: 100,
+                page: 1
+            },
+            isUpdate: false,
+            rules: {
+                name: [
+                    { required: true, message: '请输入角色名称', trigger: 'blur' },
+                    { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+                ],
+            },
+            roleList: [],
+            dialogFormVisible: false,
+            form: {
+                select:1,
+                value: [1, 4]
+            },
+            data: generateData(),
+
+        };
+    },
+    methods: {
+        del() {},
+        handleUpdate() {},
+        updata(){},
+        switchChange() {},
+        save() {},
+        getData() {
+        },
+        addRole() {
+            this.dialogFormVisible = true;
+        }
+    }
 };
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+.role {
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    border-radius: 4px;
 
+    .role-top-header {
+        height: 50px;
+        border-bottom: 1px solid #EAEDF7;
+    }
+
+    .search {
+        margin-top: 12px;
+    }
+
+    .table-box {
+        padding: 0 20px;
+        .switch {
+            .open {
+                width: 6px;
+                height: 6px;
+                background: #67C23A;
+                border-radius: 50%;
+                margin-right: 6px;
+            }
+
+            .close {
+                width: 6px;
+                height: 6px;
+                background: #C0C4CC;
+                border-radius: 50%;
+                margin-right: 6px;
+            }
+        }
+        .but {
+            font-size: 12px;
+            font-family: PingFangSC-Regular, PingFang SC, sans-serif;
+            font-weight: 400;
+            color: #2A79EE;
+            line-height: 18px;
+        }
+    }
+
+    .pagination {
+        height: 100px;
+        margin: 0;
+    }
+}
+</style>
+<style>
+.role .el-dialog__body {
+    padding: 20px;
+}
+.role .el-dialog__footer .cancel {
+    background: #FFFFFF;
+    border-radius: 2px;
+    border: 1px solid #2A79EE;
+    color: #2A79EE;
+}
 </style>
--- src/pages/systemManagement/user.vue
+++ src/pages/systemManagement/user.vue
@@ -1,91 +1,91 @@
 <template>
-    <div class="container">
-        <div class="handle-box">
-            <el-input style="width: 150px; margin-right: 10px" placeholder="请输入账户名" v-model="newsTitle"
+    <div class="user">
+        <div class='user-top-header flex items-center'>
+            <div class='flex items-center ml-15'>
+                <img style='width: 22px;height: 21px' src='../../assets/img/mark.png'>
+            </div>
+            <h3 class='ml-5'>人员及权限管理</h3>
+        </div>
+        <div class="handle-box my-20 ">
+            <el-input class='ml-20' style="width: 150px; margin-right: 10px" placeholder="请输入账户名" v-model="query.account"
                       size="small" clearable />
-            <el-input style="width: 150px; margin-right: 10px" placeholder="请输入手机号" v-model="newsSource"
+            <el-input style="width: 150px; margin-right: 10px" placeholder="请输入手机号" v-model="query.phone"
                       size="small" clearable />
-            <el-select placeholder="请选择角色" v-model="checkStatus" size="small"
+            <el-select placeholder="请选择角色" v-model="query.role" size="small"
                        style="width: 150px; margin-right: 10px" clearable>
-                <el-option v-for="item in stsusOptions" :key="item.value" :label="item.label"
+                <el-option v-for="item in roleList" :key="item.value" :label="item.label"
                            :value="item.value" />
             </el-select>
-            <el-select placeholder="请选择状态" v-model="checkStatus" size="small"
+            <el-select placeholder="请选择状态" v-model="query.status" size="small"
                        style="width: 150px; margin-right: 10px" clearable>
-                <el-option v-for="item in stsusOptions" :key="item.value" :label="item.label"
+                <el-option v-for="item in statusList" :key="item.value" :label="item.label"
                            :value="item.value" />
             </el-select>
-            <el-date-picker v-model="dateRange" size="small" style="width: 240px" value-format="yyyy-MM-dd"
+            <el-date-picker v-model="query.dateRange" size="small" style="width: 240px" value-format="yyyy-MM-dd"
                             type="daterange" range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期"
                             @change="timeChange" clearable>
             </el-date-picker>
             <el-button style="margin-left: 10px" type="primary" size="small" icon="el-icon-s-search"
-                       @click="getArticleList()">查询
+                       @click="getData()">查询
             </el-button>
             <el-button style="margin-left: 10px;border: 1px solid #2A79EE;color: #2A79EE" plain  size="small" icon="el-icon-s-search"
                        @click="addUser">添加
             </el-button>
         </div>
-        <el-table :data="tableData" style="width: 100%" stripe border>
-            <el-table-column  label="账户名" min-width="500px" align="center">
-                <template slot-scope='scope'>
-					<span class='span-text' v-if='scope.row.sourceAddress!= undefined && scope.row.sourceAddress!=null'>
-						<a link style='color: #2A79EE' :href='scope.row.sourceAddress' target='_blank'>
-							<h4 v-text='scope.row.title' />
-						</a>
-					</span>
-                    <span class='span-text'
-                          v-if='scope.row.sourceAddress== undefined || scope.row.sourceAddress ==null'>
-						<h4 v-text='scope.row.title' />
-					</span>
-                </template>
-            </el-table-column>
-            <el-table-column prop="source" label="手机号" min-width="180" align="center"></el-table-column>
-            <el-table-column prop="source" label="角色"  min-width="180" align="left"></el-table-column>
-            <el-table-column prop="checkStatus" label="状态" sortable min-width="200" align="center" :formatter="setStatus">
-            </el-table-column>
-            <el-table-column prop="inputTime" label="上次登录时间" sortable min-width="220" align="center"></el-table-column>
-            <el-table-column prop="inputTime" label="创建时间" sortable min-width="220" align="center"></el-table-column>
-            <el-table-column label="操作" min-width="200" align="center" fixed="right">
-                <template #default="scope">
-                    <router-link :to="{ path: '/editNews', query: { checkID: scope.row.checkID }}">修改
-                    </router-link>
-                </template>
-            </el-table-column>
-        </el-table>
-        <el-row class="Pagination" style="text-align: left;margin-top: 10px;">
-            <el-pagination background layout="prev, pager, next" :total="total" :page-size="pageSize"
-                           :current-page="currentPage" @current-change="changePage">
+       <div class='table-box box-sizing'>
+           <el-table :data="tableData" style="width: 100%" stripe border>
+               <el-table-column  label="账户名" min-width="96" align="center"></el-table-column>
+               <el-table-column prop="source" label="手机号" min-width="133" align="center"></el-table-column>
+               <el-table-column prop="source" label="角色"  min-width="237" align="left"></el-table-column>
+               <el-table-column prop="checkStatus" label="状态" sortable min-width="115" align="center" :formatter="setStatus">
+               </el-table-column>
+               <el-table-column prop="inputTime" label="上次登录时间" sortable min-width="185" align="center"></el-table-column>
+               <el-table-column prop="inputTime" label="创建时间" sortable min-width="185" align="center"></el-table-column>
+               <el-table-column label="操作" min-width="200" align="center" fixed="right">
+                   <template #default="scope">
+                       <router-link :to="{ path: '/editNews', query: { checkID: scope.row.checkID }}">修改
+                       </router-link>
+                   </template>
+               </el-table-column>
+           </el-table>
+       </div>
+        <div class='pagination flex row-center items-center'>
+            <el-pagination
+                @size-change='getData'
+                :page-size='query.size'
+                :current-page='query.page'
+                layout='total,prev, pager, next, jumper'
+                :total='1'>
             </el-pagination>
-        </el-row>
+        </div>
 
 
 <!--        添加Dialog-->
         <el-dialog title="权限编辑" :visible.sync="outerVisible"  width='40%'>
-            <el-form :inline="true" :model="formInline" class="demo-form-inline">
+            <el-form :inline="true" :model="userForm" class="demo-form-inline">
                 <el-form-item label="账户名">
-                    <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+                    <el-input v-model="userForm.username" placeholder="审批人"></el-input>
                 </el-form-item>
                 <el-form-item label="密码">
-                    <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+                    <el-input v-model="userForm.password" placeholder="审批人"></el-input>
                 </el-form-item>
                 <el-form-item label="手机号">
-                    <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+                    <el-input v-model="userForm.phone" placeholder="审批人"></el-input>
                 </el-form-item>
                 <el-form-item label=" 状态">
-                    <el-select v-model="formInline.region" placeholder="活动区域">
+                    <el-select v-model="userForm.region" placeholder="活动区域">
                         <el-option label="区域一" value="shanghai"></el-option>
                         <el-option label="区域二" value="beijing"></el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label=" 角色">
-                    <el-select v-model="formInline.region" placeholder="活动区域">
+                    <el-select v-model="userForm.roleID" placeholder="活动区域">
                         <el-option label="区域一" value="shanghai"></el-option>
                         <el-option label="区域二" value="beijing"></el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="权限">
-                    <el-input v-model="formInline.user" placeholder="请选择权限" @click="innerVisible = true"></el-input>
+                    <el-input v-model="userForm.authority" placeholder="请选择权限" @click="innerVisible = true"></el-input>
                 </el-form-item>
             </el-form>
 
@@ -117,124 +117,59 @@ import {
 export default {
     data() {
         return {
-            outerVisible: true,//外面的弹窗
-            innerVisible: false,//里面的弹窗
-            formInline: {
-                user: '',
-                region: ''
+            query: {
+                account: '',
+                phone: '',
+                role: '',
+                status: '',
+                dateRange: '',
+                size: 10,
+                page: 1
             },
-            newsTitle: '',
-            newsSource: '',
-            newsLabel: '',
-            dateRange: [],
-            checkStatus: '',
-            wordsStatus: '',
-            total: 0, // 总条数
-            currentPage: 1, // 当前页
-            pageSize: 10, // 分页大小
+            roleList: [],
+            statusList: '',
             tableData: [],
-            stsusOptions: [{
-                value: 0,
-                label: '未审核'
-            }, {
-                value: 1,
-                label: '已通过'
-            }, {
-                value: 2,
-                label: '不通过'
-            }],
-            unPassList: []
+            userForm: {
+                username: '',
+                password: '',
+                phone: '',
+                roleID: 1,
+                authority: []
+            },
+            outerVisible: false,
+            innerVisible: false
         }
     },
     created() {
-        this.initData();
+
     },
     methods: {
-        //添加
-        addUser(){
-
+        timeChange(e) {
+            console.log(e)
         },
-        timeChange(value) {
-            if (value == null) {
-                this.dateRange = [];
-            }
-        },
-        async initData() {
-            try {
-                var res = await getChildList({
-                    pid: 4
-                })
-                if (res != null && res.code == 200) {
-                    res.data.forEach(i => {
-                        const a = {};
-                        a.value = i.id;
-                        a.label = i.name;
-                        this.unPassList.push(a);
-                    })
-                }
-
-                this.getArticleList();
-            } catch (err) {
-                console.log('获取数据失败', err);
-            }
-        },
-        changePage(val) {
-            this.currentPage = val;
-            this.getArticleList();
-        },
-        async getArticleList() {
-            try {
-                var params = {
-                    page: this.currentPage,
-                    size: this.pageSize,
-                    title: this.newsTitle,
-                    source: this.newsSource,
-                    checkStatus: this.checkStatus,
-                    unPass: this.wordsStatus == '' ? -1 : this.wordsStatus
-                }
-
-                if (this.dateRange != null && this.dateRange.length > 0) {
-                    params.startTime = this.dateRange[0];
-                    params.endTime = this.dateRange[1];
-                }
-
-                const res = await getWaitList(params);
-                if (res != null && res.code == 200) {
-                    this.tableData = res.data;
-                    this.total = res.total;
-                } else {
-                    throw new Error(res.message)
-                }
-            } catch (err) {
-                console.log('获取数据失败', err);
-            }
-        },
-        setStatus(row, column) {
-            try {
-                console.log(column)
-                const status = row[column.property];
-                if (status == null || status == '' || status == 0) {
-                    return "未审核";
-                } else if (status == 1) {
-                    return "已通过";
-                } else if (status == 2) {
-                    return "不通过";
-                }
-            } catch (e) {
-                console.log(e);
-            }
-        },
-    },
+        getData() {},
+        addUser() {},
+        setStatus() {}
+    }
 }
 </script>
 
-<style>
-.handle-box {
-    margin-bottom: 20px;
-}
-
-.table {
-    width: auto;
-    font-size: 14px;
+<style scoped lang='scss'>
+.user {
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    border-radius: 4px;
+    .user-top-header {
+        height: 50px;
+        border-bottom: 1px solid #EAEDF7;
+    }
+    .table-box {
+        padding: 0 20px;
+    }
+    .pagination {
+        height: 100px;
+        margin: 0;
+    }
 }
 </style>
